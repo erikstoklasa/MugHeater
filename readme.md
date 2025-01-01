28a3e34ae1e0982c70da5dcd629d87063e05b858
Erik Stoklasa <me@erikstoklasa.cz>
2025-01-01 21:59:32 +0100

    Update README and main.c with detailed project functionalities and peripheral connections

--- readme.md
+++ readme.md
@@ -1,4 +1,23 @@
 # Mug heater project
 - Nucleo STM32-F303K8
 ## Electornic schema
-![Schematic](image.png)
+![Schematic](schematic.png)
+
+## Peripherals connected
+- Load switching MOSFET gate on `PF1`
+- NTC Thermistor on `PA3`
+- Buzzer on `PA5` which indicates a reached temperature
+- Button on `PA7` which turns `off` and `on` the heating
+- LED on `PA4` which indicates mode of the heating
+  - Flashes and then `on` indicates starting heating
+  - Flashes and then `off` indicates stopping heating
+
+## Functionalities
+- Every 1 minute reads AD and either turns off or on the load based on the specified temperature `Default: 60°C`
+- Button turns on or off the heating
+- Plays a sound when specified temperature reached
+
+
+
+Notes:
+- Needs a 500 ohm short from NRST to 3.3V in order to operate from only one cable. Stlink pulls down the NRST pin, which causes the MC to be in reset state when stlink is not connected.
--- src/main.c
+++ src/main.c
@@ -6,10 +6,6 @@
 #define AVG_SLOPE 4.3f // Avg_Slope = 4.3 mV/°C
 #define VREFINT 3.3f   // VDDA value
 
-// ADC register bit positions for F303
-#define ADC_SQR1_SQ1_Pos 6U
-#define ADC_SMPR1_SMP16_Pos 16U
-
 #define ADC_RESOLUTION 4096  // 12-bit ADC
 #define VREF 3.3             // Reference voltage in volts
 #define R_PULLUP 10000.0     // Pull-up resistor in ohms (10kΩ)
@@ -17,6 +13,10 @@
 #define B_COEFFICIENT 4050.0 // Beta coefficient (B)
 #define T_REF 298.15         // Reference temperature in Kelvin (25°C)
 
+#define T_DESIRED 40.0f // Desired temperature in Celsius
+
+volatile uint16_t is_enabled_heating = 0;
+volatile uint16_t has_reached_temp = 0;
 /*
  * Initialize the load switch on PA11
  */
@@ -24,10 +24,13 @@ void Load_Switch_Init(void)
 {
     // enable GPIOA clock
     RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
-    // set pa11 as output
+    // set PA11 as output
     GPIOA->MODER |= GPIO_MODER_MODER11_0;
 }
 
+/*
+ * Initialize the ADC to read the temperature sensor on PA3
+ */
 void ADC_TempSensor_Init(void)
 {
     RCC->AHBENR |= RCC_AHBENR_GPIOAEN; // Enable GPIOA clock
@@ -111,27 +114,158 @@ float Convert_NTC_Temperature(uint16_t adc_value)
     return temp_celsius;
 }
 
+void Init_Button(void)
+{
+    // Init button on PA7
+    RCC->AHBENR |= RCC_AHBENR_GPIOAEN; // Enable GPIOA clock
+    // Configure PA7 as input
+    GPIOA->MODER &= ~GPIO_MODER_MODER7;  // Input mode
+    GPIOA->PUPDR |= GPIO_PUPDR_PUPDR7_0; // Pull-up
+    // Enable SYSCFG clock
+    RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
+    // Connect EXTI7 to PA7
+    SYSCFG->EXTICR[1] &= ~SYSCFG_EXTICR2_EXTI7;   // Clear EXTI7 bits
+    SYSCFG->EXTICR[1] |= SYSCFG_EXTICR2_EXTI7_PA; // Map PA7 to EXTI7
+
+    // Configure EXTI7 to trigger on rising edge
+    EXTI->RTSR |= EXTI_RTSR_TR7;
+    // Unmask EXTI7
+    EXTI->IMR |= EXTI_IMR_IM7;
+    // Enable EXTI7 interrupt
+    NVIC_EnableIRQ(EXTI9_5_IRQn);
+    NVIC_SetPriority(EXTI9_5_IRQn, 0);
+}
+
+void Init_LED(void)
+{
+    // Enable GPIOA clock
+    RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+
+    // Configure PA4 as output
+    GPIOA->MODER &= ~GPIO_MODER_MODER4_1; // Set PA4 to output mode
+    GPIOA->MODER |= GPIO_MODER_MODER4_0;
+
+    // Ensure LED is initially off
+    GPIOA->ODR &= ~GPIO_ODR_4;
+}
+
+volatile uint32_t sys_tick_count = 0;
+
+void SysTick_Handler(void)
+{
+    sys_tick_count++; // Increment the millisecond counter
+}
+
+// Function to get the current time in milliseconds
+uint32_t GetSysTick(void)
+{
+    return sys_tick_count;
+}
+
+// Delay function using SysTick
+void Delay_ms(uint32_t delay)
+{
+    uint32_t start_time = GetSysTick();
+    while ((GetSysTick() - start_time) < delay)
+        ;
+}
+
+volatile uint32_t last_interrupt_time = 0; // Track the last interrupt time
+
+// Button press handler
+void EXTI9_5_IRQHandler(void)
+{
+    // Check if EXTI7 triggered the interrupt
+    if (EXTI->PR & EXTI_PR_PR7)
+    {
+        // Clear the pending interrupt flag for EXTI7
+        EXTI->PR |= EXTI_PR_PR7;
+
+        // Get the current system tick in milliseconds (from sys_tick_count)
+        uint32_t current_time = sys_tick_count;
+
+        // Debounce: Ensure at least 50ms between interrupts
+        if ((current_time - last_interrupt_time) >= 50)
+        {                                       // 50ms debounce threshold
+            last_interrupt_time = current_time; // Update last interrupt time
+
+            // Toggle the load switch on PA11
+            GPIOA->ODR ^= GPIO_ODR_11;
+            is_enabled_heating = !is_enabled_heating;
+            // Toggle the LED on PA4
+            GPIOA->ODR ^= GPIO_ODR_4;
+        }
+    }
+}
+
+void Init_Buzzer(void)
+{
+    // Init buzzer on PA5
+    RCC->AHBENR |= RCC_AHBENR_GPIOAEN; // Enable GPIOA clock
+    // Configure PA5 as output
+    GPIOA->MODER &= ~GPIO_MODER_MODER5_1; // Output mode
+    GPIOA->MODER |= GPIO_MODER_MODER5_0;
+}
+
+/**
+ * Convert temperature to blinks on the LED
+ * @param temp: Temperature in Celsius
+ * @note 1 blink per 10 degrees
+ */
+void Convert_Temp_To_Blinks(float temp)
+{
+    // Convert temp to number of blinks 1 blink per 10 degrees
+    int num_blinks = (int)(temp / 10);
+    // Blink LED num_blinks times
+    for (int i = 0; i < num_blinks; i++)
+    {
+        GPIOA->ODR |= GPIO_ODR_4;  // Turn on LED
+        Delay_ms(500);             // Delay 500ms
+        GPIOA->ODR &= ~GPIO_ODR_4; // Turn off LED
+        Delay_ms(500);             // Delay 500ms
+    }
+}
+
 int main(void)
 {
+    // Initialize the load switch
     Load_Switch_Init();
     // Initialize temperature sensor ADC
     ADC_TempSensor_Init();
-    volatile float temperature = 0.0f;
+    Init_LED();
+    // Initialize button
+    Init_Button();
+    // Initialize buzzer and systick for it
+    Init_Buzzer();
 
+    // Setup systick timer for 1ms, use core clock
+    SysTick_Config(SystemCoreClock / 1000);
+    float temperature = 0.0f;
+    uint16_t raw_value;
     while (1)
     {
-        uint16_t raw_value = ADC1_Read();
+        raw_value = ADC1_Read();
         temperature = Convert_NTC_Temperature(raw_value);
-        if (temperature > 60.0f)
+        if ((temperature < T_DESIRED) && is_enabled_heating)
         {
-            GPIOA->ODR &= ~GPIO_ODR_11;
+            GPIOA->ODR |= GPIO_ODR_11; // Turn on load
         }
         else
         {
-            GPIOA->ODR |= GPIO_ODR_11;
+            GPIOA->ODR &= ~GPIO_ODR_11; // Turn off load
+            if (!has_reached_temp && (temperature >= T_DESIRED))
+            {
+                has_reached_temp = 1;
+                for (int i = 0; i < 200; ++i)
+                {
+                    GPIOA->ODR |= GPIO_ODR_5; // Turn on buzzer
+                    Delay_ms(1);
+                    GPIOA->ODR &= ~GPIO_ODR_5; // Turn off buzzer
+                    Delay_ms(1);
+                }
+            }
         }
-
-        // Delay between readings
-        for (volatile uint32_t i = 0; i < 100000; i++);
+        // Convert_Temp_To_Blinks(temperature);
+        Delay_ms(10000); // Check every 10s
     }
 }
